933b9c49673c69f5d877f053bd3a585e2124564b
Cristian Gutierrez <cristiandisenadorvisual@gmail.com>
2024-07-08 20:41:16 -0500

    Agregamos un Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Unicornios from "./pages/Unicornios";
+import Home from "./pages/Home";
 import { AutenticacionProveedor } from "./contexts/Autenticacion";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <AutenticacionProveedor>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/unicornios" element={<Unicornios />} />
         </Routes>
